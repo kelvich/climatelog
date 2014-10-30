241e2cac64bacc68d21c91f23b70ce0488abb0c6
Stas Kelvich <stas.kelvich@gmail.com>
2014-10-31 00:42:22 +0300

    chart fix timezones

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,17 +2,18 @@ class StatsController < ApplicationController
 
   def index
     current_hour = DateTime.now.hour
-    @labels = current_hour.
-      downto(current_hour-24).
-      map{|h| h>=0? h: 24+h}.
+    @hours = current_hour.
+      downto(current_hour-23).
+      map{|h| h%24 }.
       reverse
 
-    @temps = Measurement.
-      group('EXTRACT(HOUR FROM created_at)').
-      order('EXTRACT(HOUR FROM created_at)').
-      average(:temperature).
-      values.
-      map(&:to_f)
+    temps = Measurement.
+      group('EXTRACT(HOUR FROM created_at)::int').
+      order('EXTRACT(HOUR FROM created_at)::int').
+      average(:temperature)
+
+    @values = @labels.
+      map{ |h| @temps[ (h-3)%24 ].to_f }
 
   end
 
