bb1f1f090fdf211fd8297aeb811121ca4d036626
Stas Kelvich <stas.kelvich@gmail.com>
2014-10-30 22:43:12 +0300

    temp graph

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,19 @@
 class StatsController < ApplicationController
+
   def index
+    current_hour = DateTime.now.hour
+    @labels = current_hour.
+      downto(current_hour-24).
+      map{|h| h>=0? h: 24+h}.
+      reverse
+
+    @temps = Measurement.
+      group('EXTRACT(HOUR FROM created_at)').
+      order('EXTRACT(HOUR FROM created_at)').
+      average(:temperature).
+      values.
+      map(&:to_f)
+
   end
+
 end
